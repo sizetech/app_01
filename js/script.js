2b6273858311e5bb3b8c184a1c19722df039bd32
sizetech <contato@sizetech.com.br>
2014-10-28 17:11:26 -0200

    v0.00

--- js/script.js
+++ js/script.js
@@ -80,6 +80,9 @@ function editar_meus_dados(){
 
 }
 
+
+
+
 $(function(){
 	var validar = idmorador();
 	var ancora = window.location.hash;
@@ -133,11 +136,11 @@ function meusanimais(){
 						
 						lista += ' <li style="padding-left:0; min-height:0" data-role="collapsible" data-theme="d" data-iconpos="right" data-inset="false">';
 						lista +=	'<h2>'+data[x].nome_animal;
-						lista +=	'<img src="img/icones/icone-bino.png" align="right" style="margin: 0px 0px;">';
+						lista +=	'<img src="img/btn/animais.png" align="right" style="margin: 0px 0px;">';
 						lista +=	'</h2>';
 						lista +=	'<form >';
 						lista +=	  '<fieldset data-role="controlgroup" data-type="horizontal">';
-						lista +=		'<a href="#form_meus_animais" onClick="editarAnimal('+data[x].ID+');" id="btn-list-detalhe" class="ui-btn ui-corner-all">Detalhes/Editar</a>';
+						lista +=		'<a href="#form_meus_animais" onClick="editar_meus_animais('+data[x].ID+');" id="btn-list-detalhe" class="ui-btn ui-corner-all">Detalhes/Editar</a>';
 						lista +=		'<a href="#" onClick="excluirAnimal('+data[x].ID+');" id="btn-list" class="ui-btn ui-corner-all">Excluir</a>	';
 						lista +=	 ' </fieldset>';
 						lista +=	'</form>';
@@ -147,6 +150,7 @@ function meusanimais(){
 					$('#listadeanimais').html(lista);
 					$('#listadeanimais').listview("refresh");
 		}, 'json');
+						
 
 }
 // funções de inicialização
@@ -157,3 +161,22 @@ function meusanimais(){
 				meusanimais();
 			}
 	//fim
+	function editar_meus_animais(){
+	$.post(URLBASE+'meus_animais.php', $('#formulario_meus_animais').serialize(), function(data) {
+					$.each( data, function( ) {
+					
+						meusanimais();
+											
+						if(data.status){
+							$('.msgsucesso_meus_animais p').html(data.mensagem);
+							$('.msgsucesso_meus_animais').css('display','block');
+						}else{
+							$('.msgerro_meus_animais p').html(data.mensagem)
+							$('.msgerro_meus_animais').css('display','block');
+						}
+						window.location = "#meus_animais";
+					
+					});
+	})
+
+}
