0026d5a092a62317b87d1d6ca48daf424c73279f
Álvaro Felipe <alvarofelipems@gmail.com>
2015-03-03 13:21:53 -0300

    Condomínio Fácil

--- js/funcoes/panico.js
+++ js/funcoes/panico.js
@@ -10,7 +10,7 @@ function ativarPanico(){
 			pararPanico();
 		}
 	 },            // callback to invoke with index of button pressed
-    'iCondominio',           // title
+    'Condomínio Fácil',           // title
     ['Sim','Não']     // buttonLabels
 );
 
@@ -33,7 +33,7 @@ function ativarPanico(){
 			$('.login').css('background','#112C3C');
 		}
 	 },            // callback to invoke with index of button pressed
-    'iCondominio',           // title
+    'Condomínio Fácil',           // title
     ['Sim','Não']     // buttonLabels
 );
 	}
